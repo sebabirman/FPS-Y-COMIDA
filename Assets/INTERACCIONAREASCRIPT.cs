c3da22e6eb4559a82334e344a3adc1c79c566090
sebabirman <152617869+sebabirman@users.noreply.github.com>
2024-06-10 16:05:06 -0300

    Initial commit

---
+++ Assets/INTERACCIONAREASCRIPT.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+public class INTERACCIONAREASCRIPT : MonoBehaviour
+{
+    int puntaje;
+    public Text txtpuntaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PickUpScript pickup;
+        pickup = other.GetComponent<PickUpScript>();
+        puntaje += pickup.puntosItem;
+        txtpuntaje.text = "Puntaje" + puntaje;
+
+        if(pickup.esComida == true)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FPS-Y-COMIDA
+ 
